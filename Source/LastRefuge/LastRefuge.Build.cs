c4fb306e58d9f5d321fc5b430771fb22c96e8a30
jjinzxx <epspqm823@gmail.com>
2026-05-08 22:15:20 +0900

    chore: 프로젝트 초기화

---
+++ Source/LastRefuge/LastRefuge.Build.cs
@@ -0,0 +1,44 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class LastRefuge : ModuleRules
+{
+	public LastRefuge(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"LastRefuge",
+			"LastRefuge/Variant_Horror",
+			"LastRefuge/Variant_Horror/UI",
+			"LastRefuge/Variant_Shooter",
+			"LastRefuge/Variant_Shooter/AI",
+			"LastRefuge/Variant_Shooter/UI",
+			"LastRefuge/Variant_Shooter/Weapons"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/LastRefuge/LastRefugeGameMode.h
@@ -0,0 +1,22 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LastRefugeGameMode.generated.h"
+
+/**
+ *  Simple GameMode for a first person game
+ */
+UCLASS(abstract)
+class ALastRefugeGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ALastRefugeGameMode();
+};
+
+
+
